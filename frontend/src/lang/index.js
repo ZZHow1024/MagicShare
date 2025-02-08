51b2a1dbd2de67a3a9d38ddcfd13c43ee114daf0
ZZHow <mail@zzhow.com>
2025-02-08 15:29:09 +0800

    feat: add language adjustment based on browser settings on frontend

--- frontend/src/lang/index.js
+++ frontend/src/lang/index.js
@@ -3,8 +3,19 @@ import zhHans from './modules/zh-Hans.js'
 import zhHant from './modules/zh-Hant.js'
 import enUs from './modules/en-US.js'
 
+let language = navigator.language || navigator.userLanguage
+
+if (language.includes('zh')) {
+  if (language.includes('CN') || language.includes('cn')) language = 'zh-Hans'
+  else if (language.includes('Hans') || language.includes('hans')) language = 'zh-Hans'
+  else if (language.includes('Hant') || language.includes('hant')) language = 'zh-Hant'
+  else language = 'zh-Hant'
+} else {
+  language = 'en-US'
+}
+
 const vueI18n = createI18n({
-  locale: 'en-US',
+  locale: language,
   messages: {
     'zh-Hans': {
       ...zhHans,
